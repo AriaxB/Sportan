bf839c8571f0938a0c22e3bf6f9a50d803a61552
jhonyx <jnba0529@gmail.com>
2023-09-08 21:42:27 -0500

    12

---
+++ nicolas/src/test/java/sportann/nicolas/NicolasApplicationTests.java
@@ -0,0 +1,13 @@
+package sportann.nicolas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
